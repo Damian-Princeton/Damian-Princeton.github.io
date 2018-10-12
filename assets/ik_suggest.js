472a7d593d6142a9b0f0bd927e484758108fe0ad
Damian Sian <dsian@princeton.edu>
2018-10-12 09:52:06 -0400

    five

--- assets/ik_suggest.js
+++ assets/ik_suggest.js
@@ -1,19 +1,3 @@
-;(function ( $, window, document, undefined ) {
- 
-/**
-* Create a paragraph with version number for JS file for testing 
-*/
-
-var para = document.createElement("P");                       // Create a <p> element
-var t = document.createTextNode("Version 3");       // Create a text node
-para.appendChild(t);                                          // Append the text to <p>
-document.body.appendChild(para);                              // Append <p> to <body>
-
-/**
-* Changing doc title for version control
-*/
-document.title = "Version Four";
-
 var pluginName = "ik_suggest",
 	defaults = {
 		'instructions': "As you start typing the application might suggest similar search terms. Use up and down arrow keys to select a suggested search string.",
